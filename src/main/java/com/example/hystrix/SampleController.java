873b15a45414a0b43ecbf701966c107e59cd6a3c
J-minkuk <mingood92@gmail.com>
2021-02-01 20:01:38 +0900

    Hystrix log

--- src/main/java/com/example/hystrix/SampleController.java
+++ src/main/java/com/example/hystrix/SampleController.java
@@ -7,20 +7,24 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * 설명 :
  *
- * @author Hardy(조민국) / mingood92@gmail.com
+ * @author Minkuk Jo / mingood92@gmail.com
  * @since 2021. 01. 14
  */
 @RestController
 @RequestMapping("sample")
 public class SampleController {
 
+    private static int count = 1;
+
     @GetMapping("/server2")
     public String sample() {
+        System.out.println("## hystrix server2 case1");
         return "Server2 Call Success";
     }
 
     @GetMapping("/server2-fallback")
     public String sampleFallback() {
+        System.out.println("## [" + (count++) + "] hystrix server2 case2");
         try {
             Thread.sleep(1000L);
         } catch (InterruptedException e) {
